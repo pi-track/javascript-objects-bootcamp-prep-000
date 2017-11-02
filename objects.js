1fe1bf05414a82918cc4d0e23a6c517d1c41b534
patrick.eells@gmail.com <patrick.eells@gmail.com>
2017-11-02 17:55:48 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,7 +7,7 @@ function updatePlaylist(obj,key,value){
   return Object.assign({}, obj, { [key]: value })
 }
 
-function removeFromPlaylist(obj,key){
-  newobj = delete obj.key
-  return obj;
+function removeFromPlaylist(playlist,artistName){
+  delete playlist.artistName
+  return playlist
 }
