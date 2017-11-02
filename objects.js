e94195d3fb50a29e3ba6c8818642a6218c4d913c
patrick.eells@gmail.com <patrick.eells@gmail.com>
2017-11-02 17:33:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,6 +3,6 @@ var playlist = {
   velvet: "perfect day",
 };
 
-function updatePlaylist(obj,artist,song){
+function updatePlaylist(obj,key,value){
   return Object.assign({}, obj, { [key]: value })
 }
